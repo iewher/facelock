4a1093c88fd8a63499d4c8541b7ef58ca4cbcd40
Georgy <georgy-solodovnikov1@yandex.ru>
2026-07-17 21:43:18 +0500

    Add sqlite database

---
+++ lib/main/database.ts
@@ -0,0 +1,15 @@
+import Database from 'better-sqlite3'
+import { app } from 'electron'
+import path from 'path'
+
+const getDbPath = (): string => {
+  const userDataPath = app.getPath('userData')
+  return path.join(userDataPath, 'facelock.db')
+}
+
+export const db = new Database(getDbPath(), {
+  verbose: process.env.NODE_ENV !== 'production' ? console.log : undefined,
+})
+
+db.pragma('journal_mode = WAL')
+db.pragma('foreign_keys = ON')
